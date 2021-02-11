64b2eecb15ed8ef27c7fd3629b3be38f3720b186
Kanthesha Devaramane <kanthesh.dc@gmail.com>
2021-02-11 13:35:43 +0000

    dynamically set erc1820 contract address

--- contracts/ERC1820Client.sol
+++ contracts/ERC1820Client.sol
@@ -11,19 +11,26 @@ contract ERC1820Registry {
 
 /// Base client to interact with the registry.
 contract ERC1820Client {
-    ERC1820Registry constant ERC1820REGISTRY = ERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+    ERC1820Registry internal erc1820Registry;
+    constructor (address erc1820ContractAddress) internal {
+        if (erc1820ContractAddress != address(0)) {
+            erc1820Registry = ERC1820Registry(erc1820ContractAddress);
+        } else {
+            erc1820Registry = ERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
+        }
+    }
 
     function setInterfaceImplementation(string memory _interfaceLabel, address _implementation) internal {
         bytes32 interfaceHash = keccak256(abi.encodePacked(_interfaceLabel));
-        ERC1820REGISTRY.setInterfaceImplementer(address(this), interfaceHash, _implementation);
+        erc1820Registry.setInterfaceImplementer(address(this), interfaceHash, _implementation);
     }
 
     function interfaceAddr(address addr, string memory _interfaceLabel) internal view returns(address) {
         bytes32 interfaceHash = keccak256(abi.encodePacked(_interfaceLabel));
-        return ERC1820REGISTRY.getInterfaceImplementer(addr, interfaceHash);
+        return erc1820Registry.getInterfaceImplementer(addr, interfaceHash);
     }
 
     function delegateManagement(address _newManager) internal {
-        ERC1820REGISTRY.setManager(address(this), _newManager);
+        erc1820Registry.setManager(address(this), _newManager);
     }
 }
